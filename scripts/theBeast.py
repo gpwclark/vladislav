1260678f67da70ed1556b74c1163185ea1721c21
gpwclark <gpwclark@gmail.com>
2016-07-19 20:00:53 -0400

    theBeast seems to pass tests, need to do more testing and finish commenting at the top

--- scripts/theBeast.py
+++ scripts/theBeast.py
@@ -5,7 +5,7 @@ import os
 import time
 import requests
 
-# The Beast's job is to take the name of the torrent and monitor 
+# The Beast's job is to take the name of the torrent and monitor
 # the path_to_watch for that torrent's corresponding download. When the
 # torrent's corresponding download is finished scp-ing to the path_to_watch
 # (New files are scp-ed to the path_to_watch when a given torrent has finished
@@ -22,22 +22,22 @@ import requests
 #       TRIGGERS: vlad awaken the beast {{Filename}} has been uploaded to dropbox.
 #           |-> theBeast.py runs waiting for {{Filename}}
 # 2. EVENT: torrent file appears in /home/emby/movies/torrents/
+#       TRIGGERS:
 #
 #
-#
-#
+#TODO make sure we send slack messages if something goes poorly.
 
-class theBeast:        
-    def __init__(self, m_file, ifttt_api_key):
-        self.subsetTolerance = .5
-        self.timeToWait = 2160 
-        self.path_to_watch = "/home/emby/movies/torrents/"
+class theBeast:
+    def __init__(self, m_file, ifttt_api_key, path_to_watch):
+        self.subset_tolerance = .5
+        self.time_to_wait = 2160
+        self.path_to_watch = path_to_watch
         self.m_file = m_file
         self.m_file_as_set = self.stringToSet(m_file)
         self.ifttt_api_key = ifttt_api_key
         self.api_endpoint = "https://maker.ifttt.com/trigger/torrent_ready/with/key/" + self.ifttt_api_key
         self.api_endpoint_talk = "https://maker.ifttt.com/trigger/talk_to_vlad/with/key/" + self.ifttt_api_key
-        payload = {'value1': "don't call me that arsehole"}
+        payload = {'value1': "don't call me that arsehole."}
         r = requests.post(self.api_endpoint_talk, data=payload)
 
     def stringToSet(self, string):
@@ -50,7 +50,7 @@ class theBeast:
     # The self.m_file is the torrent file. A potential match (as in a
     # downloaded file that appears in path_to_watch)in the added list
     # will not have the exact same name as the self.m_file but it will
-    # be more or less a subset, e.g. 
+    # be more or less a subset, e.g.
     # self.m_file: Alex G - EASY - 2011 (WEB - MP3 - V0 (VBR))-31511159.torrent
     # and it's corresponding file:
     # in added: Alex G - EASY - 2011 - V0
@@ -60,9 +60,9 @@ class theBeast:
         origLen = len(folderSet)
         intersecLen = len(folderSet.intersection(self.m_file_as_set))
         ratio = float(intersecLen) / float(origLen)
-        if (ratio > self.subsetTolerance):
+        if (ratio > self.subset_tolerance):
             return True
-        
+
     def containsFilename(self, added):
         for elem in added:
             print ("the elem: ")
@@ -76,12 +76,17 @@ class theBeast:
         r = requests.post(self.api_endpoint, data=payload)
         print("remem pending cmd: ", r.status_code,r.reason)
 
-    def get_size(self, start_path):
-        total_size = 0
-        for dirpath, dirnames, filenames in os.walk(start_path):
-            for f in filenames:
-                fp = os.path.join(dirpath, f)
-                total_size += os.path.getsize(fp)
+    def get_size(self, filePath):
+        total_size = os.path.getsize(filePath)
+        if os.path.isfile(filePath):
+            return total_size
+        else:
+            for item in os.listdir(filePath):
+                itempath = os.path.join(filePath, item)
+                if os.path.isfile(itempath):
+                    total_size += os.path.getsize(itempath)
+                elif os.path.isdir(itempath):
+                    total_size += self.get_size(itempath)
         return total_size
 
     def waitForXfer(self, dir_to_monitor):
@@ -90,7 +95,6 @@ class theBeast:
         payload = {'value1': forgetcmd}
         r = requests.post(self.api_endpoint, data=payload)
         print("forget downloading cmd: ", r.status_code, r.reason)
-        #TODO WAIT FUNCTIONALITY GOES HERE
         filepath = self.path_to_watch + dir_to_monitor
         #TODO turn 60 and 10 into wait_period and non_changing_threshold
         count = 0
@@ -99,6 +103,7 @@ class theBeast:
 
         #TODO need to verify all pahts of this method are traversed
         while (count < 60):
+            print("before: ", before)
             time.sleep(2)
             after = self.get_size(filepath)
             if before == after:
@@ -107,6 +112,7 @@ class theBeast:
                 if same_count == 10:
                     return True
             else:
+                print("change")
                 same_count = 0
             before = after
             count = count + 1
@@ -118,7 +124,7 @@ class theBeast:
         return False
 
     def monitor(self):
-        fileTransferring = True 
+        fileTransferring = True
         foundMatch = False
         count = 0
 
@@ -134,11 +140,11 @@ class theBeast:
 
         # fileTransferring is false when the file is found, and
         # 2160 is the number of 10 second increments that equals
-        # 6 hours, which is as long as I want this script to 
+        # 6 hours, which is as long as I want this script to
         # wait. After 12 hours that file is probably not coming.
 
         print ("monitoring")
-        while (fileTransferring and (count < self.timeToWait)):
+        while (fileTransferring and (count < self.time_to_wait)):
             count = count + 1
             time.sleep (10)
             after = dict ([(f, None) for f in os.listdir (self.path_to_watch)])
@@ -154,14 +160,14 @@ class theBeast:
                     fileTransferring = False
                     if self.waitForXfer(foundMatch):
                         self.xferFinished(foundMatch)
-            if removed: 
+            if removed:
                 removedStr = "Removed: ", ", ".join (removed)
                 payload = {'value1': removedStr}
                 r = requests.post(self.api_endpoint, data=payload)
                 print(r.status_code, r.reason)
             before = after
 
-        if (count == self.timeToWait):
+        if (count == self.time_to_wait):
             # tell vlad to remember the file was not found.
             rememcmd = "remem " + "\"" + self.m_file + "\"" " is failed_xfer"
             payload = {'value1': rememcmd}
@@ -169,9 +175,11 @@ class theBeast:
 
 
 if __name__ == "__main__":
-    beast = theBeast(m_file=sys.argv[1],ifttt_api_key=sys.argv[2])
-    #beast.monitor()
-    beast.waitForXfer("francis")
+    beast = theBeast(m_file = sys.argv[1],
+            ifttt_api_key = sys.argv[2],
+            path_to_watch = sys.argv[3])
+    beast.monitor()
+    #beast.waitForXfer("francis blah")
 
 # Original monitoring script: http://timgolden.me.uk/python/win32_how_do_i/watch_directory_for_changes.html
 #import os, time
